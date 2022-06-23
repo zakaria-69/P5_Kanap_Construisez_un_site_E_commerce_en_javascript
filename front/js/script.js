25dae6764bd3c5d5dc1c123c3a49b4da06ee852b
zakaria-69 <ladjrafi_zakaria@yahoo.com>
2022-06-23 03:00:50 +0200

    appel API et intégration des éléments page Accueil dans le DOM

---
+++ front/js/script.js
@@ -0,0 +1,56 @@
+
+//création d'un tableau vide pour y insérer les éléments//
+let itemsData = [];
+
+//Appel API via la méthode fetch (get)//
+const fetchItemsData = () => {
+    fetch("http://localhost:3000/api/products")
+    .then ((res) => res.json())
+    .then ((promise)=>  {
+//on passe les données transformée dans le tableau vide//
+        itemsData=promise;
+        console.log(itemsData);
+
+    //création d'une boucle for pour crée les élements et y intégrer les données de l'API//
+
+    for(i=0;i<itemsData.length;i++){
+
+      let itemsLink =document.createElement('a');
+      itemsLink.append();
+      itemsLink.href=`href=./product.html?id=${itemsData[i]._id}` ;
+
+      let itemsImages=document.createElement('img');
+      itemsImages.append();
+      itemsImages.src=`${itemsData[i].imageUrl}`; 
+      itemsImages.alt=`${itemsData[i].altTxt}`;
+  
+      let itemsTitle=document.createElement('h3');
+      itemsTitle.append(`${itemsData[i].name}`);
+    
+      let itemsDescription=document.createElement('p');
+      itemsDescription.append(`${itemsData[i].description}`);
+
+  //on inclus dans Article les balises img h3 et p pour respecter la hiérarchie HTML"
+      let itemsArticle=document.createElement('article');
+      itemsArticle.append(itemsImages,itemsTitle,itemsDescription);
+
+//on inclus dans a(itemsLink) articles et les balises le composant pour réspecter la même hiérarchie HTML"
+
+      itemsLink.append(itemsArticle);
+
+      //on injecte le tout dans le DOM"
+      items.append(itemsLink);
+
+     
+    }})
+
+   
+}
+
+console.log(items);
+//on appel la fonction réalisée plus haut//
+fetchItemsData();
+
+
+
+
